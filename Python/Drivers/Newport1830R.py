499a40c22a2d33e9eca510be22372391460c1b7f
Daniel Hutama <daniel.hutama@mail.mcgill.ca>
2021-11-17 12:51:08 -0500

    Increased functionality of Newport1830R PM.

--- Python/Drivers/Newport1830R.py
+++ Python/Drivers/Newport1830R.py
@@ -15,8 +15,47 @@ class Newport1830R(Connection):
         Connection.__init__(self,addressString)
         self.Manual = 'Newport1830R.pdf'
         
-    def GetSTR(self, cmd):
-        res = self.__query(cmd)
-        return res
+    def GetInfo(self):
+        try:
+            idn = self.__query__('*IDN?')
+            return idn
+        except:
+            print('<< ERROR: Unable to get device information. Check connectivity. >>')
 
+    def Reset(self):
+        try:
+            self.__write__('*RST')
+        except:
+            print('<< ERROR: Unable to send reset command. Check connectivity. >>' )
+
+        
+    def GetSTR(self, cmd):
+        res = self.__query__(cmd)
+        return res
+    
+    def AttenuatorON(self):
+        self.__write__('AO')
+        
+    def AttenuatorOFF(self):
+        self.__write__('A1')
+        
+    def GetStatusAttenuator(self):
+        res = self.__query__('A?')
+        return res
+    
+    def GetWavelength(self):
+        res = self.__query__('W?')
+        return round(float(res), 4)    
+    
+    def SetWavelength(self, val):
+        strval = str(val)
+        if len(strval) == 3:
+            strval = '0' + strval
+        elif len(strval == 4):
+            pass
+        else:
+            print('<< ERROR: Value out of range. Enter wavelength in nm. >>')
+        self.__write__('W{}'.format(strval))
+        
+    
     #placeholder for driver to be completed later
