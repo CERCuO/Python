db270bf594f5209f5e05ba3d868dc35c520e0cc5
Daniel Hutama <daniel.hutama@mail.mcgill.ca>
2021-11-29 16:02:06 -0500

    Added some default functions to AWG610.m driver

--- Matlab/Tektronix_AWG610.m
+++ Matlab/Tektronix_AWG610.m
@@ -2,6 +2,11 @@ classdef Tektronix_AWG610
     % driver for Tektronix AWG 610 arbitrary waveform generator
     % Developed by Daniel Hutama
     % Version 0.1 11 Nov 2021
+    % visadev needs Matlab R2021a or newer to work.
+    
+    % This device uses IEEE 488.2 
+    % Standard Commands for Programmable Instruments (SCPI) 
+    
     
     properties
         obj
@@ -16,14 +21,27 @@ classdef Tektronix_AWG610
             else 
                 AWG.info = '';
             end 
-            AWG.model = 'Tektronix AWG 610'
-            
-            AWG.obj = visadev(VisaAddr)
+            AWG.model = 'Tektronix AWG 610';
+            AWG.manualURL = 'https://download.tek.com/manual/070A81050.pdf';
+            AWG.obj = visadev(VisaAddr);
         end 
+        
+        function ClearDevice(AWG)
+            AWG.writeline('CLS');
+        end 
+        
+        function FlushDevice(AWG)
+            AWG.flush()
+        end 
+        
         
         function GetInfo(AWG)
             tmp = query(AWG.obj, '*IDN?');
             disp(tmp)
+        end 
+        
+        function SendData(AWG)
+            
         end 
         
     end 
