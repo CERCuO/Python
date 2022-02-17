47b152c19fda6d938164dddb4f9eaef05c2bc72e
Daniel Hutama <daniel.hutama@mail.mcgill.ca>
2022-02-16 19:22:16 -0500

    Added additional query functionality for Thorlabs cam.

--- Core/Drivers/Thorlabs_DCC1240M.py
+++ Core/Drivers/Thorlabs_DCC1240M.py
@@ -8,7 +8,9 @@ Created on Thu Feb  3 11:31:09 2022
 # driver for Thorlabs DCC124M
 # developed by Daniel Hutama
 # dhuta087@uottawa.ca
+
 # Version 00.100 | 03 Feb 2022 | Initial build
+# Version 00.200 | 16 Feb 2022 | Additional query functionality added
 
 #https://www.thorlabs.com/software_pages/ViewSoftwarePage.cfm?Code=ThorCam
 
@@ -18,6 +20,7 @@ from pylablib.devices import uc480 as cam
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 
 
@@ -28,6 +31,17 @@ class Thorlabs_DCC1240M():
         self.camID = cam.uc480.find_by_serial(self.SN)
         self.obj = cam.UC480Camera(self.camID)
         self.helplink = 'https://pylablib.readthedocs.io/en/latest/devices/uc480.html#cameras-uc480'
+        self.helplin2 = 'https://pylablib.readthedocs.io/en/latest/.apidoc/pylablib.devices.uc480.html#module-pylablib.devices.uc480'
+    
+    def GetInfoDevice(self):
+         now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+         print('{}   |   <GET INFO DEVICE>'.format(now))                   
+         return self.obj.get_device_info()
+     
+    def GetInfoAcquisition(self):
+         now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+         print('{}   |   <GET INFO ACQUISITION>'.format(now))                   
+         return self.obj.get_acquisition_parameters()
     
     def GetExposure(self):
          now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -45,14 +59,56 @@ class Thorlabs_DCC1240M():
          print('{}   |   <SNAP PHOTO>'.format(now))           
          return self.obj.snap()
          
+    def Open(self):
+         now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+         print('{}   |   <OPEN CAMERA>'.format(now))                   
+         self.obj.open()        
+     
+    def Close(self):
+         now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+         print('{}   |   <CLOSE CAMERA>'.format(now))                   
+         self.obj.close()
     #gain, saturation, denoising, extrema scaling,  
+    
+    def GetDetectorSize(self):
+         now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+         print('{}   |   <GET DETECTOR PIXEL SIZE>'.format(now))                   
+         return self.obj.get_detector_size()        # returns tuple (width, height)
         
         
-def ShowImage(image, cmap = 0):
-     if cmap == 1:
-         return plt.imshow(image)
-     elif cmap == 0:
-         plt.gray()
-         plt.imshow(image)
-         plt.show()
+    def ShowImage(image, cmap = 0):
+         now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+         print('{}   |   <DISPLAY IMAGE>'.format(now))           
+         figure(figsize = (10,10))
+         if cmap == 1:
+             return plt.imshow(image)
+         elif cmap == 0:
+             plt.gray()
+             plt.imshow(image)
+             plt.show()
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
          
