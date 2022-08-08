75151c28cb18b2ef4a6d8d72883e09710891a7d8
Daniel Hutama <daniel.hutama@mail.mcgill.ca>
2022-08-08 08:36:41 -0400

    2450 skeleton

---
+++ Core/Drivers/Tektronix_2450_Sourcemeter.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 08:31:48 2022
+
+@author: danhu
+"""
+
+# import sys
+# sys.path.append('C:\\depot\\CERC\\Python\\Core\\Drivers\\inst_specific_files')
+
+
+# driver for Tektronix 2450 Sourcemeter
+# developed by Daniel Hutama
+# dhuta087@uottawa.ca
+# Version 00.100 | 08 Aug 2022 | Initial structure setup
+
+# This devices uses IEEE 488.2
+# Standard Commands for Programmable Instruments (SCPI)
+
+#be sure to add "drivers" and "dependencies" folder to your machine's syspath
+# github.com/CERCuO/Python
+# The next 3 lines are specific for my computer, where I dumped all the github files into \depot\CERC
+import sys
+sys.path.append('C:\\depot\\CERC\\Python\\Core\\Dependencies')
+sys.path.append('C:\\depot\\CERC\\Python\\Core\\Drivers')
+
+
+# you may need to run this class twice if modules are not properly loaded.
+
+from __connection__ import Connection
+from datetime import datetime
+import numpy as np
+
+class KoshinKogaku_LS610A(Connection):
+    def __init__(self,addressString):
+        Connection.__init__(self,addressString)
+        self.Manual = 'Tek_2450_Sourcemeter.pdf'
+        
+    def GetInfo(self):
+        try:
+            idn = self.__query__('*IDN?')
+            return idn
+        except:
+            print('<< ERROR: Unable to get device information. Check connectivity. >>')
+            
+    def GetSTR(self, cmd):
+        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print('{}   |   <{}>'.format(now, cmd.upper()))      
+        res = self.__query__(cmd)
+        return res
+    
